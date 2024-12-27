9f853a4dbbafe08c31ab3d9cc25dde496e7feb1d
Dhruvil0037 <dhruvildhamecha2003@gmail.com>
2024-12-27 14:28:19 +0530

    projec setup intialized

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Button className="bg-red-700">Check</Button>
+    </>
+  )
+}
+
+export default App
